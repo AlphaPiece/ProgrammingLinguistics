9b36d3b03db9ea37e4bec0c97dd8dc39d00545d7
Zexi Wang <twopieces0921@gmail.com>
2019-06-09 23:42:45 -0400

    finished 1-23

--- Scheme/SICP/exercises/chap1/1-22.scm
+++ Scheme/SICP/exercises/chap1/1-22.scm
@@ -71,10 +71,10 @@
 ;;; The three samllest primes larger than 1000000:
 ;;; 1000003, 1000033, 1000037.
 
- (search-for-primes 1000000000 1000000021)       ; 1e9 
- (search-for-primes 10000000000 10000000061)     ; 1e10 
- (search-for-primes 100000000000 100000000057)   ; 1e11 
- (search-for-primes 1000000000000 1000000000063) ; 1e12
+(search-for-primes 1000000000 1000000021)       ; 1e9 
+(search-for-primes 10000000000 10000000061)     ; 1e10 
+(search-for-primes 100000000000 100000000057)   ; 1e11 
+(search-for-primes 1000000000000 1000000000063) ; 1e12
 
 ;;; Output:
 ;;;
