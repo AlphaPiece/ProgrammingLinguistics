d27a36565013fbe8a8fcf6fab828be5178e9d707
Zexi Wang <twopieces0921@gmail.com>
2019-06-22 09:14:31 -0400

    finished 1-30

--- Scheme/SICP/exercises/chap1/1-30.scm
+++ Scheme/SICP/exercises/chap1/1-30.scm
@@ -5,9 +5,30 @@
 ;;; iteratively. Show how to do this by filling in the missing
 ;;; expressions in the following definition:
 
+(define (sum-recur term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum-recur term (next a) next b))))
 
-(define (sum term a next b)
+(define (sum-iter term a next b)
   (define (iter a result)
-    (if <??>
-        <??>
-        (iter <??> <??>))) (iter <??> <??>))
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(define (cube x)
+  (* x x x))
+
+(define (inc n)
+  (+ n 1))
+
+(define (sum-cubes-recur a b)
+  (sum-recur cube a inc b))
+
+(define (sum-cubes-iter a b)
+  (sum-iter cube a inc b))
+
+(sum-cubes-recur 1 10)
+(sum-cubes-iter 1 10)
