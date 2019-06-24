d7ece255b8f0b2c1cf890238091cc70464bb4332
Zexi Wang <twopieces0921@gmail.com>
2019-06-23 21:19:44 -0400

    finished 1-38

---
+++ Scheme/SICP/exercises/chap1/1-38.scm
@@ -0,0 +1,32 @@
+;;;; Exercise 1.38
+
+;;; In 1737, the Swiss mathematician Leonhard Euler published
+;;; a memoir De Fractionibus Continuis, which included a continued
+;;; fraction expansion for e - 2, where e is the base of the
+;;; natural logarithms. In this fraction, the N_i are all 1,
+;;; and the D_i are successively
+;;;
+;;; 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, ....
+;;;
+;;; Write a program that uses your cont-frac procedure from
+;;; exercise 1.37 to approximate e, based on Euler’s expansion.
+
+(define (cont-frac n d k)
+  (define (recur i)
+    (if (= i k)
+        (/ (n k) (d k))
+        (/ (n i) (+ (d i)
+                    (recur (+ i 1))))))
+  (recur 1))
+
+(define euler-e
+  (+ (cont-frac (lambda (i) 1.0)
+                (lambda (i)
+                  (let ((j (+ i 1)))
+                    (if (= (remainder j 3) 0)
+                        (* (/ j 3) 2)
+                        1)))
+                20)
+     2))
+
+euler-e
