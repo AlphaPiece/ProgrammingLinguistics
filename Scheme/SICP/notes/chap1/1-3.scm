a879ff3fc6f8e55d12aabfb9b0a352fad79c5249
Zexi Wang <twopieces0921@gmail.com>
2019-06-25 13:45:07 -0400

    updated notes

--- Scheme/SICP/notes/chap1/1-3.scm
+++ Scheme/SICP/notes/chap1/1-3.scm
@@ -148,3 +148,59 @@
   (fixed-point (lambda (y) (average y (/ x y)))
                1.0))
 
+;;; 1.3.4
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+((average-damp square) 10)
+
+;;; Method 1 for computing square root of x:
+;;; Look for a fixed point of the average-damped
+;;; version of y ↦ x/y.
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+;;; Method 2 for computing square root of x:
+;;; Use Newton’s method that finds a fixed point of
+;;; the Newton transform of y ↦ y^2 - x.
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+;;; Method 1
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+
+;;; Method 2
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            newton-transform
+                            1.0))
