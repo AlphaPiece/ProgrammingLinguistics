2d8fb56d14d6f9c23e8591c13adcda99da2c38e3
Zexi Wang <twopieces0921@gmail.com>
2019-07-08 16:54:46 -0400

    updated notes 2-2

--- Scheme/SICP/notes/chap2/2-2.scm
+++ Scheme/SICP/notes/chap2/2-2.scm
@@ -61,3 +61,29 @@ l2
       (cons (car l1) (append (cdr l1) l2))))
 
 (append l1 l2)
+
+(define nil '())
+
+(define (scale-list items factor)
+  (if (null? items)
+      nil
+      (cons (* (car items) factor)
+            (scale-list (cdr items) factor))))
+
+(scale-list (list 1 2 3 4 5) 10)
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6 17))
+(map (lambda (x) (* x x))
+     (list 1 2 3 4))
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
+
+(scale-list (list 1 2 3 4 5) 10)
