9a2344a625d4f329b4c7cea9e164e7a4fdeb22a5
Zexi Wang <twopieces0921@gmail.com>
2019-07-26 19:19:22 -0400

    updated notes 2-3

---
+++ Scheme/SICP/notes/chap2/2-3.scm
@@ -0,0 +1,73 @@
+;;;; Notes for Section 2.3
+
+;;;
+;;; 2.3.1
+;;;
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+
+(memq 'apple '(x (apple sauce) y apple pear))
+
+;;;
+;;; 2.3.2
+;;;
+
+(define (deriv expr var)
+  (cond ((number? expr) 0)
+        ((variable? expr)
+         (if (same-variable? expr var) 1 0))
+        ((sum? expr)
+         (make-sum (deriv (addend expr) var)
+                   (deriv (augend expr) var)))
+        ((product? expr)
+         (make-sum
+           (make-product (multiplier expr)
+                         (deriv (multiplicand expr) var))
+           (make-product (deriv (multiplier expr) var)
+                         (multiplicand expr))))
+        (else
+          (error "unknown experssion type -- DERIV" expr))))
+
+(define (variable? e) (symbol? e))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (addend e) (cadr e))
+(define (augend e) (caddr e))
+(define (sum? e)
+  (and (pair? e) (eq? (car e) '+)))
+
+(define (make-product m1 m2) (list '* m1 m2))
+(define (multiplier e) (cadr e))
+(define (multiplicand e) (caddr e))
+(define (product? e)
+  (and (pair? e) (eq? (car e) '*)))
+
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+(define (=number? expr num)
+  (and (number? expr) (= expr num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
+
