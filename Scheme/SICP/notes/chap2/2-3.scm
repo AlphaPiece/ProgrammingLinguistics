c59760045a8a9e00d6d5ed94d8009d495cc26e86
Zexi Wang <twopieces0921@gmail.com>
2019-07-29 17:59:36 -0400

    updated notes 2-3

--- Scheme/SICP/notes/chap2/2-3.scm
+++ Scheme/SICP/notes/chap2/2-3.scm
@@ -77,18 +77,49 @@
 
 ;;; Sets as unordered lists
 
+;;; Let n be the size of set.
+
+;;; Θ(n) steps
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
         (else (element-of-set? x (cdr set)))))
 
+;;; Θ(n) steps
 (define (adjoin-set x set)
   (if (element-of-set? x set)
       set
       (cons x set)))
 
+;;; Assume that both set1 and set2 have the same size n.
+
+;;; Θ(n^2) steps
 (define (intersection-set set1 set2)
   (cond ((or (null? set1) (null? set2)) '())
         ((element-of-set? (car set1) set2)
-         (cons (car set1) (intersection-set (cdr set1) set2)))
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
+
+;;; Sets as ordered lists
+
+;;; Θ(n) steps
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+;;; Θ(n) steps
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((> x1 x2)
+               (intersection-set set1 (cdr set2)))))))
